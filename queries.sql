9c69e207e20f147c8b7e42e0213046683039ba8b
Wiktor Czetyrbok <kasztanators2701@gmai.com>
2023-01-19 12:47:15 +0100

    select

--- queries.sql
+++ queries.sql
@@ -1,23 +1,21 @@
---1 GIVE NUMBER OF PACKAGES SENDED BY EVERY USER
+--1 GIVE NUMBER OF FRAGILE PACKAGES SENDED BY EVERY USER 
 
 SELECT name,surname, COUNT(Packages.packageID) AS 'Number of packages'
 	FROM Users, Packages, Orders
 	WHERE Packages.orderREF = orderID AND
-	Orders.userREF = Users.userID
+	Orders.userREF = Users.userID AND Packages.is_fragile =1
 	GROUP BY Users.name,surname
 	ORDER BY name
 
 
 --2Find the average weight of packages sent to a specific city:
-SELECT AVG(weight) AS 'average package weight in Torun', COUNT(PackageID) AS 'No of packages to city'
+SELECT AVG(weight) AS 'average package weight', COUNT(PackageID) AS 'No of packages to city',city
 FROM Packages
 JOIN Addresses ON Packages.addressREF = Addresses.addressID
-WHERE Addresses.city = 'Torun'
-
+GROUP BY city
 --- 3Find all of the tracking information for packages sent to a specific receiver 
 SELECT Tracking_info.trackerID, Tracking_info.message_CHAR, name
 	FROM Tracking_info
-
 	JOIN Packages ON Tracking_info.packageREF = Packages.packageID
 	JOIN Receivers ON Packages.receiverREF = Receivers.receiverID
 	WHERE Packages.weight < (SELECT AVG(Packages.weight)FROM Packages)
@@ -34,28 +32,29 @@ WHERE Addresses.region = 'Kujawsko-Pomorskie' AND Transports.status = 'picked'
 AND Transports.x_coor < (SELECT AVG(Transports.x_coor) FROM Transports)
 AND Transports.y_coor < (SELECT AVG(Transports.y_coor) FROM Transports )
 
---5Find the most common delivery unit that was used for a package delivery:
-		SELECT Delivery_units.unitID, COUNT(Delivery_units.unitID) AS 'frequency'
+--5Find the most commonly used delivery unit that was used for a package delivery:
+		SELECT Delivery_units.unitID, COUNT(Delivery_units.unitID) AS 'frequency',city
 		FROM Delivery_units
 		JOIN Transports ON Transports.fromUnitREF = Delivery_units.unitID 
-		GROUP BY Delivery_units.unitID
+		JOIN Addresses ON Addresses.addressID = Delivery_units.addresREF
+		GROUP BY Delivery_units.unitID, Addresses.city
 		ORDER BY COUNT(Delivery_units.unitID) DESC
 
----6 package info crate view
-drop VIEW Package_info
+---6  crate view of number of packages by users where weight is greater than average 
+DROP VIEW Package_over_average
 
-CREATE VIEW Package_info AS
-SELECT Packages.packageID, Packages.size_X, Packages.size_Y, Packages.size_Z, Packages.weight, Orders.date_of_check_in, Users.user_name
+CREATE VIEW Package_over_average AS
+SELECT Users.user_name ,COUNT(Packages.packageID) AS 'number Packages over average'
 FROM Packages
 JOIN Orders ON Orders.orderID = Packages.orderREF
-JOIN Users ON Users.userID = Orders.userREF;
+JOIN Users ON Users.userID = Orders.userREF
+WHERE Packages.weight >(SELECT AVG(weight) FROM Packages)
+GROUP BY(user_name)
 
-
-SELECT * FROM Package_info
-WHERE weight > 20	
-ORDER BY Package_info.date_of_check_in DESC
-
--- 7crating view of transport destination where destination delivery unit is he lowest
+SELECT * FROM Package_over_average
+ORDER BY(user_name)
+-- 7 transport destination where destination delivery unit capacity is the lowest
+-- after 2020
 DROP VIEW Transport_destination
 
 CREATE VIEW Transport_destination AS
@@ -67,20 +66,13 @@ FROM Transports
 	JOIN Addresses toAddress ON toUnit.addresREF = toAddress.addressID
 	WHERE toUnit.capacity = (SELECT MIN(Delivery_units.capacity)FROM Delivery_units
 		JOIN Transshipments ON Transshipments.unitREF = Delivery_units.unitID
-		WHERE Transshipments.date > '2021-12-21 09:28:32')
+		WHERE Transshipments.date > '2021-01-01 00:00:00')
 
 
 SELECT * FROM Transport_destination
-
-
-
 --8subqueries
-SELECT DISTINCT user_name ,
-(SELECT COUNT(packageID) FROM Packages WHERE Packages.orderREF = Orders.orderID) AS 'Number of Packages'
-FROM Users
-JOIN Orders ON Users.userID = Orders.userREF
 
-SELECT  DISTINCT user_name, COUNT(packageID)AS 'Number of Packages'
+SELECT user_name, COUNT(packageID)AS 'Number of Packages'
     FROM Users , Packages,Orders 
     WHERE Packages.orderREF = Orders.orderID
     AND  Orders.userREF = Users.userID
@@ -88,3 +80,19 @@ SELECT  DISTINCT user_name, COUNT(packageID)AS 'Number of Packages'
 --9 2nd subquery date
 SELECT * FROM Users 
 WHERE userID IN (SELECT userREF FROM Orders WHERE date_of_check_in >= '2020-01-01')
+
+-- 10 number of deliverymen which can drive available vehicles
+ SELECT COUNT(Deliverymen.deliverymanID) AS 'Number of available drivers'
+ FROM Deliverymen
+ JOIN Driverlogs ON Deliverymen.deliverymanID = Driverlogs.deliverymanREF
+ JOIN Vehicles ON Vehicles.vehicleID = Driverlogs.vehicleREF
+ WHERE Vehicles.is_available = 1
+ 
+ --11 POD KTOREGO MIASTA NAJCZESCIEJ PRZYJEZZDA£ POJAZD O TAKIEJ REJSTRACJI
+ SELECT city , register_plate
+ FROM Addresses
+ JOIN Packages ON addressREF  = Addresses.addressID
+ JOIN PackageTransport ON packageREF = Packages.packageID
+ JOIN Transports ON transportID = PackageTransport.packageREF
+ JOIN Vehicles ON Vehicles.vehicleID = transportID
+
